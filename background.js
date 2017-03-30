1ae56910a711e5531c90c5a68495c0806ae94e36
Garston Tremblay <gtremblay@rallydev.com>
2017-03-30 13:26:45 -0600

    Fetch number of chat messages in a Flow for Rally artifacts and display in badge

--- background.js
+++ background.js
@@ -1,7 +1,31 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+var ajaxGet = (uri, callback) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://' + API_KEY + '@api.flowdock.com/flows/' + ORG_FLOW + '/' + uri, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+            callback(JSON.parse(xhr.responseText));
+        }
+    };
+    xhr.send();
+};
+
+var setBadgeText = (text, tabId) => chrome.browserAction.setBadgeText({ tabId, text: '' + text });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
     if(changeInfo.status === 'loading') {
-        chrome.tabs.get(tabId, function (tab) {
-            chrome.browserAction.setBadgeText({ tabId: tabId, text: '' + Math.random() });
+        setBadgeText('', tabId);
+
+        chrome.tabs.get(tabId, tab => {
+            var tabUrl = tab.url.replace(/(rally1.rallydev.com\/)#\/\d+/, '$1slm/#');
+
+            ajaxGet('threads?application=399', threads => {
+                var thread = threads.find(t => t.external_url === tabUrl);
+                if(thread) {
+                    ajaxGet('threads/' + thread.id + '/messages?app=chat', messages => {
+                        setBadgeText(messages.length, tabId);
+                    });
+                }
+            });
         });
     }
 });
