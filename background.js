368f14d072fedc8df283a77be700d6f0ed6ed40b
Garston Tremblay <gtremblay@rallydev.com>
2017-03-30 13:30:44 -0600

    Refactoring out fetchChatMessages(...)

--- background.js
+++ background.js
@@ -5,6 +5,19 @@ var HOST_METADATA = {
     }
 };
 
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+    if(changeInfo.status === 'loading') {
+        setBadgeText('', tabId);
+
+        chrome.tabs.get(tabId, tab => {
+            var metadata = HOST_METADATA[tab.url.replace(/[a-z]+:\/\/([^/]+).*/, '$1')];
+            if(metadata) {
+                fetchChatMessages(metadata, tab);
+            }
+        });
+    }
+});
+
 var ajaxGet = (uri, callback) => {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://' + API_KEY + '@api.flowdock.com/flows/' + ORG_FLOW + '/' + uri, true);
@@ -16,25 +29,17 @@ var ajaxGet = (uri, callback) => {
     xhr.send();
 };
 
+var fetchChatMessages = ({ appId, urlTransformer }, { id, url }) => {
+    url = urlTransformer(url);
+
+    ajaxGet('threads?application=' + appId, threads => {
+        var thread = threads.find(t => t.external_url === url);
+        if(thread) {
+            ajaxGet('threads/' + thread.id + '/messages?app=chat', messages => {
+                setBadgeText(messages.length, id);
+            });
+        }
+    });
+};
+
 var setBadgeText = (text, tabId) => chrome.browserAction.setBadgeText({ tabId, text: '' + text });
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    if(changeInfo.status === 'loading') {
-        setBadgeText('', tabId);
-
-        chrome.tabs.get(tabId, tab => {
-            var metadata = HOST_METADATA[tab.url.replace(/[a-z]+:\/\/([^/]+).*/, '$1')];
-            if(metadata) {
-                ajaxGet('threads?application=' + metadata.appId, threads => {
-                    var url = metadata.urlTransformer(tab.url);
-                    var thread = threads.find(t => t.external_url === url);
-                    if(thread) {
-                        ajaxGet('threads/' + thread.id + '/messages?app=chat', messages => {
-                            setBadgeText(messages.length, tabId);
-                        });
-                    }
-                });
-            }
-        });
-    }
-});
