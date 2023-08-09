d8987f781f22cfe69a8ff01e9a057d67c2c35b45
Garston Tremblay <garstont+github@gmail.com>
2023-08-08 21:05:24 -0600

    [PhysEd] only clear cached game status when navigating to new thread (to prevent posts when a thread can't be read for some reason)

--- PhysEd/background.js
+++ PhysEd/background.js
@@ -27,19 +27,21 @@
                 return;
             }
 
+            const navigatingToThread = null;
             const scriptMsgPrefix = `${scriptName} -`
             const reminderMsgPrefix = `Reminder: ${scriptMsgPrefix}`;
             const selectorThreadPane = '.p-workspace__secondary_view';
 
             const tabs = await chrome.tabs.query({url: 'https://app.slack.com/*'});
             const threads = await executeScript(tabs, slackGetThread, [{
+                navigatingToThread,
                 reminderMsgPrefix,
                 selectorThreadPane
             }]);
 
             const threadMsgs = threads.map(t => {
                 if (!t) {
-                    return;
+                    return t;
                 }
 
                 const existingMsgs = cachedThreadMsgs[t.id];
@@ -57,7 +59,9 @@
 
             if (threadMsgs.some(t => !t)) {
                 console.log('not all tabs are ready', threads, threadMsgs);
-                cachedGameStatus = emptyGameStatusObj();
+                if (threadMsgs.includes(navigatingToThread)) {
+                    cachedGameStatus = emptyGameStatusObj();
+                }
                 return;
             }
 
@@ -149,7 +153,7 @@
             // need to navigate to thread by clicking b/c setting window.location.href will cause Slack to redirect to channel URL when thread has no messages
             threadStarter.dispatchEvent(new MouseEvent('mouseover', {'bubbles': true}));
             getMsgCt(threadStarter).querySelector('[data-qa="start_thread"]').click();
-            return;
+            return consts.navigatingToThread;
         }
 
         return {
